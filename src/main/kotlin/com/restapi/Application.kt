23018caa1b9dab3b9b45f241f48f0f2dc98da7fe
Alison Yutaka Inoue <alisoninoue>
2021-03-21 18:26:50 +0000

    Initial commit

---
+++ src/main/kotlin/com/restapi/Application.kt
@@ -0,0 +1,21 @@
+package com.restapi
+
+import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "demorest",
+            version = "0.0"
+    )
+)
+object Api {
+}
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("com.restapi")
+		.start()
+}
+
